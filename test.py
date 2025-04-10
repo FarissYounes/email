7c06c127860123932ef862c4cf6f2a456b1fb0a7
Frobinus <131169277+FarissYounes@users.noreply.github.com>
2025-04-10 14:53:56 +0200

    Update test.py

--- test.py
+++ test.py
@@ -9,6 +9,63 @@
 # En cas de code HTTP 200, 301 ou 302, il affiche l'URL trouvée.
 #
 
+
+from elasticsearch import Elasticsearch
+
+# --- Config ---
+ES_HOST = 'http://localhost:9200'
+INDEX_NAME = 'your_index_name'
+UUID_FIELD = 'uuid.keyword'
+
+es = Elasticsearch(ES_HOST)
+
+def count_duplicate_uuids_composite(index, uuid_field):
+    duplicate_count = 0
+    after_key = None
+
+    while True:
+        agg_query = {
+            "size": 0,
+            "aggs": {
+                "duplicate_uuids": {
+                    "composite": {
+                        "size": 1000,
+                        "sources": [
+                            {"uuid": {"terms": {"field": uuid_field}}}
+                        ],
+                        **({"after": after_key} if after_key else {})
+                    },
+                    "aggs": {
+                        "uuid_count": {
+                            "value_count": {"field": uuid_field}
+                        }
+                    }
+                }
+            }
+        }
+
+        response = es.search(index=index, body=agg_query)
+        buckets = response['aggregations']['duplicate_uuids']['buckets']
+        
+        for bucket in buckets:
+            if bucket['doc_count'] > 1:
+                duplicate_count += 1
+
+        if 'after_key' in response['aggregations']['duplicate_uuids']:
+            after_key = response['aggregations']['duplicate_uuids']['after_key']
+        else:
+            break
+
+    return duplicate_count
+
+# --- Run ---
+if __name__ == '__main__':
+    count = count_duplicate_uuids_composite(INDEX_NAME, UUID_FIELD)
+    print(f"Total duplicated UUIDs: {count}")
+
+
+
+
 if [ "$#" -ne 2 ]; then
     echo "Usage : $0 <BASE_URL> <WORDLIST>"
     exit 1
